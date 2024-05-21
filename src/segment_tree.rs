1412f5cc13fe809bcd958deefadacc33dae86fd4
Mathis Rech <mathisrech@gmx.de>
2024-05-22 01:46:45 +0200

    Refactor segment tree

--- src/segment_tree.rs
+++ src/segment_tree.rs
@@ -9,7 +9,7 @@ impl<T> Segment<T> {
     #[inline]
     fn len(&self) -> i32 {
         assert!(self.end_inclusive >= self.start_inclusive);
-        self.end_inclusive - self.start_inclusive + 1
+        self.end_inclusive - self.start_inclusive
     }
 }
 
@@ -39,21 +39,20 @@ impl<T> SegmentTree<T> {
         self.segments.clear();
         self.segments.extend(segments.into_iter());
         self.segments
-            .par_sort_unstable_by_key(|segment| segment.end_inclusive);
+            .par_sort_unstable_by_key(|segment| segment.start_inclusive);
         self.max_segment_len = self.segments.iter().map(Segment::len).max().unwrap_or(0);
     }
 
     fn find_start_index(&self, position: i32) -> usize {
-        match self
-            .segments
-            .binary_search_by(|segment| segment.end_inclusive.cmp(&position))
-        {
+        match self.segments.binary_search_by(|segment| {
+            (segment.start_inclusive + self.max_segment_len).cmp(&position)
+        }) {
             Ok(mut index) => loop {
                 if index == 0 {
                     return 0;
                 }
 
-                if self.segments[index].end_inclusive >= position {
+                if (self.segments[index].start_inclusive + self.max_segment_len) >= position {
                     index -= 1;
                 } else {
                     return index + 1;
@@ -64,12 +63,13 @@ impl<T> SegmentTree<T> {
     }
 
     fn find_end_index(&self, position: i32) -> usize {
-        match self.segments.binary_search_by(|segment| {
-            (segment.end_inclusive - self.max_segment_len).cmp(&position)
-        }) {
+        match self
+            .segments
+            .binary_search_by(|segment| segment.start_inclusive.cmp(&position))
+        {
             Ok(mut index) => {
                 while let Some(segment) = self.segments.get(index) {
-                    if (segment.end_inclusive - self.max_segment_len) < position {
+                    if segment.start_inclusive <= position {
                         index += 1;
                     } else {
                         break;
@@ -89,11 +89,14 @@ impl<T> SegmentTree<T> {
         #[cfg(debug_assertions)]
         {
             if let Some(start_segment) = self.segments.get(start_index) {
-                assert!(start_segment.end_inclusive >= position);
+                assert!((start_segment.start_inclusive + self.max_segment_len) >= position);
             }
 
             if let Some(before_start_index) = start_index.checked_sub(1) {
                 if let Some(before_start_segment) = self.segments.get(before_start_index) {
+                    assert!(
+                        (before_start_segment.start_inclusive + self.max_segment_len) < position
+                    );
                     assert!(before_start_segment.end_inclusive < position);
                 }
             }
@@ -105,8 +108,8 @@ impl<T> SegmentTree<T> {
 
         self.segments[start_index..end_index]
             .iter()
-            .filter(move |segment| segment.start_inclusive <= position)
-            .inspect(move |segment| debug_assert!(segment.end_inclusive >= position))
+            .filter(move |segment| segment.end_inclusive >= position)
+            .inspect(move |segment| debug_assert!(segment.start_inclusive <= position))
             .map(|segment| &segment.value)
     }
 }
